82aa3c768ecc4df81e81978e41e0693104de186a
Natasha-rare <natasha_ea5@mail.ru>
2023-05-12 22:01:09 +0300

    case model added

--- ssl_website/classes/models.py
+++ ssl_website/classes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 import datetime
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinLengthValidator
 
 User = get_user_model()
 # Create your models here.
@@ -14,11 +15,18 @@ def get_closest_game():
     closest_sat = today + datetime.timedelta(6 -(today.weekday() + 1) % 7)
     return closest_sat
 
+# посещаемость игрока – отметка пришел/опоздал/пропустил игру
 class Attendance(models.TextChoices):
     PLAYED = "Сыграл"
     SKIP = "Не пришел"
     LATE = "Опоздал"
 
+
+'''
+Модель для регистрации игрока на игру
+Хранит в себе данные об игре
+Отображается в лк пользователя
+'''
 class GameRegister(models.Model):
     date = models.DateField(null=False, default=get_closest_game())
     player = models.ForeignKey(to=User, on_delete=models.CASCADE, to_field="id")
@@ -33,9 +41,16 @@ class GameRegister(models.Model):
         verbose_name="Посещаемость",
     )
 
+
+'''
+Модель для создании игры
+Хранит в данные об игроках (связь с таблицей case) и конкретном кейсе (связь с таблицей Cases)
+Отображается в сетке
+(В дальнейшем будет использована для просмотра во вкладке "прошедшие игры")
+'''
 class Game(models.Model):
     date = models.DateField(null=False, default=get_closest_game())
-    case_number = models.IntegerField("Номер кейса", default=0, null=False)
+    case_number = models.IntegerField("Номер кейса", default=0, null=False)  # сделать связь с моделью Cases – ?
     player_1 = models.ForeignKey(GameRegister, related_name="player_1", on_delete=models.CASCADE)
     player_1_score = models.IntegerField("Счет 1 игрока", default=0, null=False)
     player_2 = models.ForeignKey(GameRegister, related_name="player_2", on_delete=models.CASCADE)
@@ -48,7 +63,34 @@ class Game(models.Model):
         verbose_name="Тип игры",
     )
 
-
+'''
+Модель для хранении информации о судье
+Судья привязан к столу, дате и типу игры
+Связан с пользователями через поле referee
+'''
 class Referee(models.Model):
     referee = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    game_info = models.ManyToManyField(Game)
+    # game_info = models.ManyToManyField(Game)
+    date = models.DateField(null=False, default=get_closest_game())
+    table_number = models.IntegerField("Стол", default=0, null=False)
+    game_type = models.CharField(
+        max_length=15,
+        choices=GAME_TYPES,
+        default="CONFLICT",
+        verbose_name="Тип игры",
+    )
+
+
+'''
+Модель для хранения кейсов
+(тип, название и текст кейса)
+'''
+class Cases(models.Model):
+    case_type = models.CharField(
+        max_length=15,
+        choices=GAME_TYPES,
+        default="CONFLICT",
+        verbose_name="Тип игры",
+    )
+    name = models.CharField("Название кейса", max_length=250, null=False)
+    text = models.TextField("Текст кейса", null=False, validators=[MinLengthValidator(10)])
